4b98a0edfde1502c05d171e93178c11db9054925
jolomunar <munarjolo9@gmail.com>
2023-01-23 15:38:30 +0800

    global-committ

--- app/src/main/java/apc/mobprog/myqrcodeandbarcodescanner/GlobalBarcode.java
+++ app/src/main/java/apc/mobprog/myqrcodeandbarcodescanner/GlobalBarcode.java
@@ -5,9 +5,6 @@ import java.util.ArrayList;
 public class GlobalBarcode {
     public static String barcode;
     public static ArrayList<String> arrayList = new ArrayList<String>();
-    public static String fName;
-    public static String outlet;
-    public static String mobNum;
     public static String stCode;
     public static String size;
     public static String color;
