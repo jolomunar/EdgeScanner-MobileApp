1e5186464202e12ae006216c1af19954ba24999d
jolomunar <munarjolo9@gmail.com>
2023-06-22 12:22:58 +0800

    MasterListUpdate

--- app/src/main/java/apc/mobprog/myqrcodeandbarcodescanner/ItemMasterList.java
+++ app/src/main/java/apc/mobprog/myqrcodeandbarcodescanner/ItemMasterList.java
@@ -11,6 +11,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import android.os.Bundle;
+import android.widget.SearchView;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,8 +27,14 @@ public class ItemMasterList extends AppCompatActivity {
     MasterListAdapter imListAdapter;
     List<String> bcMasterList;
     HashMap<String, List<String>> infoMasterList;
+    SearchView searchView;
     private static final String TAG = "";
     private static final String url = "https://edgescanner.herokuapp.com/api/test-upload";
+
+    String barcode;
+    String sizeCode;
+    String colorCode;
+    String itemNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +53,35 @@ public class ItemMasterList extends AppCompatActivity {
         imListAdapter = new MasterListAdapter(this, bcMasterList, infoMasterList);
         masterList.setAdapter(imListAdapter);
 
+        searchView = findViewById(R.id.search);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Handle query submission (e.g., perform a search)
+                handleSearch(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Handle query text changes (e.g., filter the data)
+                handleSearch(newText);
+
+                searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+                    @Override
+                    public boolean onClose() {
+                        // Clear the search results and restore the original data
+                        imListAdapter = new MasterListAdapter(ItemMasterList.this, bcMasterList, infoMasterList);
+                        masterList.setAdapter(imListAdapter);
+                        return false;
+                    }
+                });
+                return false;
+            }
+        });
+
+
     }
 
     @Override
@@ -60,23 +97,24 @@ public class ItemMasterList extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         try {
+
                             JSONArray jsonArray = new JSONArray(response);
 
                             for (int i = 0; i < jsonArray.length(); i++) {
                                 JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                GlobalBarcode.barcode = jsonObject.getString("barcode_number");
-                                GlobalBarcode.size = jsonObject.getString("size_code");
-                                GlobalBarcode.color = jsonObject.getString("color_code");
-                                GlobalBarcode.stCode = jsonObject.getString("item_number");
+                                barcode = jsonObject.getString("barcode_number");
+                                sizeCode = jsonObject.getString("size_code");
+                                colorCode = jsonObject.getString("color_code");
+                                itemNumber = jsonObject.getString("item_number");
                                 // Retrieve other properties as needed
 
                                 // Add the retrieved data to your data structures
-                                bcMasterList.add(GlobalBarcode.barcode);
+                                bcMasterList.add(barcode);
                                 List<String> childItems = new ArrayList<>();
-                                childItems.add("Size Code: " + GlobalBarcode.size);
-                                childItems.add("Color Code: " + GlobalBarcode.color);
-                                childItems.add("Item Number: " + GlobalBarcode.stCode);
-                                infoMasterList.put(GlobalBarcode.barcode, childItems);
+                                childItems.add("Size Code: " + sizeCode);
+                                childItems.add("Color Code: " + colorCode);
+                                childItems.add("Item Number: " + itemNumber);
+                                infoMasterList.put(barcode, childItems);
                             }
 
                             runOnUiThread(new Runnable() {
@@ -114,4 +152,38 @@ public class ItemMasterList extends AppCompatActivity {
         bcMasterList.clear();
         infoMasterList.clear();
     }
+
+    private void handleSearch(String query) {
+        List<String> filteredBcMasterList = new ArrayList<>();
+        HashMap<String, List<String>> filteredInfoMasterList = new HashMap<>();
+
+        // Filter the data based on the query
+        for (String barcode : bcMasterList) {
+            List<String> childItems = infoMasterList.get(barcode);
+            List<String> filteredChildItems = new ArrayList<>();
+
+            // Check if the barcode matches the query
+            if (barcode.toUpperCase().contains(query.toUpperCase())) {
+                filteredBcMasterList.add(barcode);
+                filteredInfoMasterList.put(barcode, childItems);
+            } else {
+                // Check if any child value matches the query
+                for (String childItem : childItems) {
+                    if (childItem.toUpperCase().contains(query.toUpperCase())) {
+                        filteredChildItems.add(childItem);
+                    }
+                }
+
+                if (!filteredChildItems.isEmpty()) {
+                    filteredBcMasterList.add(barcode);
+                    filteredInfoMasterList.put(barcode, filteredChildItems);
+                }
+            }
+        }
+
+        // Update the adapter with filtered data
+        imListAdapter = new MasterListAdapter(this, filteredBcMasterList, filteredInfoMasterList);
+        masterList.setAdapter(imListAdapter);
+    }
+
 }
